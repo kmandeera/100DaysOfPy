7ceac6d0e57a0bd4719c1420192e330a9a2d2522
Dinith Perera <94979426+IamMrDP@users.noreply.github.com>
2023-05-27 13:39:04 +0530

    Update twinkle.py
    add description

--- twinkle.py
+++ twinkle.py
@@ -1,1 +1,7 @@
+"""
+File: twinkle.py
+Description: The provided string is formatted with proper indentation using escape sequences such as newline (\n) and tab (\t). 
+Date: May 27, 2023
+"""
+
 print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
